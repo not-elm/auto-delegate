81a8ecccfa029f357d98efd09151a6901fd23423
elmga <elmgameinfo@gmai..com>
2023-05-07 23:11:08 +0900

    FIX: テストケースのコメントアウトの外し忘れ

--- tests/progress.rs
+++ tests/progress.rs
@@ -4,7 +4,7 @@ fn tests() {
     let t = trybuild::TestCases::new();
     t.pass("tests/01-generate_impl_macro_empty_trait.rs");
     t.pass("tests/02-delegate_by_nothing_args.rs");
-    // t.pass("tests/03-generate_impl_macro_with_input.rs");
-    // t.pass("tests/03-generate_impl_macro_with_input");
+    t.pass("tests/03-delegate_by_with_args.rs");
+    t.pass("tests/04-delegate_multiple_methods.rs");
     // t.pass("tests/02-delegate_by_nothing_args");
 }
