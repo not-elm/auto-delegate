3f2e2523a4cda89d8c9aded718452dfad452f9cd
elmga <elmgameinfo@gmai..com>
2023-05-07 23:09:56 +0900

    Add: 返り値がないメソッドのテストケースの追加

--- macros/src/trait_item/trait_fn_meta.rs
+++ macros/src/trait_item/trait_fn_meta.rs
@@ -2,7 +2,6 @@ use proc_macro2::Ident;
 use syn::__private::TokenStream2;
 use syn::{ReturnType, TraitItemFn, Type, TypePath};
 
-
 use crate::trait_item::trait_fn_inputs::TraitFnInputs;
 
 pub struct TraitFnMeta(TraitItemFn);
@@ -16,14 +15,14 @@ impl TraitFnMeta {
 
     pub fn expand_fn(&self) -> syn::Result<TokenStream2> {
         let fn_name = self.fn_name();
-        let output_ty = self.output_ty();
+        let output = self.output_ty();
         let fn_inputs = TraitFnInputs::new(self.0.sig.inputs.clone());
 
         let args = fn_inputs.expand_args()?;
         let inputs = fn_inputs.expand_inputs();
+
         Ok(quote::quote! {
-            // TODO 引数やレシーバ
-            fn #fn_name(#args) -> #output_ty{
+            fn #fn_name(#args) #output{
                 self.$delegate_field.#fn_name(#inputs)
             }
         })
@@ -35,9 +34,9 @@ impl TraitFnMeta {
     }
 
 
-    fn output_ty(&self) -> Option<&Ident> {
+    fn output_ty(&self) -> Option<TokenStream2> {
         if let ReturnType::Type(_, type_path) = &self.0.sig.output {
-            return_ty(type_path)
+            return_ty(type_path).map(|ty| quote::quote! {-> #ty})
         } else {
             None
         }
---
+++ tests/04-delegate_multipe_fields.rs
@@ -0,0 +1,51 @@
+use macros::{delegate_trait, Delegate};
+
+/// 複数のメソッド、返り値がないメソッドを持つトレイトを委譲できるようにします。
+#[delegate_trait]
+trait Transformable {
+    fn move_to_x(&mut self, x: usize);
+    fn move_to_y(&mut self, y: usize);
+}
+
+
+struct Child {
+    x: usize,
+    y: usize,
+}
+
+
+impl Child {
+    pub const fn new() -> Self {
+        Self { x: 0, y: 0 }
+    }
+}
+
+
+impl Transformable for Child {
+    fn move_to_x(&mut self, x: usize) {
+        self.x = x;
+    }
+
+    fn move_to_y(&mut self, y: usize) {
+        self.y = y;
+    }
+}
+
+#[derive(Delegate)]
+struct Parent {
+    #[by(Transformable)]
+    child: Child,
+}
+
+
+fn main() {
+    let mut parent = Parent {
+        child: Child::new(),
+    };
+
+    parent.move_to_x(3);
+    assert_eq!(parent.child.x, 3);
+
+    parent.move_to_y(5);
+    assert_eq!(parent.child.y, 5);
+}
