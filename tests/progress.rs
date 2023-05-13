dee026c917249ef6ccbbc72db296f9d74268270c
elm-register <elmgameinfo@gmail.com>
2023-05-13 17:52:35 +0900

    UPDATE: 引数と返り値を参照で返せることをテスト

--- tests/progress.rs
+++ tests/progress.rs
@@ -6,4 +6,5 @@ fn tests() {
     t.pass("tests/test2_nested_modules.rs");
     t.pass("tests/test3_delegate_with_args.rs");
     t.pass("tests/test4_delegate_tuple.rs");
+    t.pass("tests/test5_delegate_reference.rs");
 }
---
+++ tests/test5_delegate_reference.rs
@@ -0,0 +1,56 @@
+use auto_delegate_macros::{Delegate, delegate_trait};
+
+/// 複数のメソッド、返り値がないメソッドを持つトレイトを委譲できるようにします。
+#[delegate_trait]
+trait StringRef {
+    fn str_ref(&self) -> &str;
+
+    fn str_mut(&mut self) -> &mut str;
+
+    fn str_change(&mut self, new_val: &str) -> &str;
+}
+
+
+struct Child {
+    str: String,
+}
+
+
+impl Default for Child {
+    fn default() -> Self {
+        Self {
+            str: String::from("Hello World!")
+        }
+    }
+}
+
+impl StringRef for Child {
+    fn str_ref(&self) -> &str {
+        self.str.as_str()
+    }
+
+    fn str_mut(&mut self) -> &mut str {
+        self.str.as_mut_str()
+    }
+
+    fn str_change(&mut self, new_val: &str) -> &str {
+        self.str = String::from(new_val);
+        self.str_ref()
+    }
+}
+
+
+#[derive(Delegate, Default)]
+struct Parent {
+    #[by(StringRef)]
+    child: Child,
+}
+
+
+fn main() {
+    let mut parent = Parent::default();
+
+    assert_eq!(parent.str_ref(), "Hello World!");
+    assert_eq!(parent.str_change("New Val"), "New Val");
+    assert_eq!(parent.str_mut(), "New Val");
+}
